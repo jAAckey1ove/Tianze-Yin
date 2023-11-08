b011c8c074552e3772885bc6ef3f188d531f3509
jAAckey1ove <yintz1207@gmail.com>
2023-11-08 13:35:29 -0600

    adding my accomplishment

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -19,29 +19,26 @@ const Portfolio = () => {
           <div className = 'portfolio__item-image'>
             <img src={img1} alt = ''/>
           </div>
-          <h3>put the achivement here</h3>
+          <h3>Cayley math contest honur roll</h3>
           <div className = 'portfolio__item-cta'>
-            <a href = '' className = 'btn' target = '_blank'>Learn More</a>
-            <a href = '' className = 'btn btn-primary' target = '_blank'>Learn More</a>
+            <a href = 'https://www.cemc.uwaterloo.ca/contests/past_contests/2023/2023FGHResults.pdf' className = 'btn btn-primary' target = '_blank'>Learn More</a>
           </div>
         </article>
         <article className = 'portfolio__item'>
           <div className = 'portfolio__item-image'>
             <img src={img1} alt = ''/>
           </div>
-          <h3>put the achivement here</h3>
+          <h3>Digi text program</h3>
           <div className = 'portfolio__item-cta'>
-            <a href = '' className = 'btn' target = '_blank'>Learn More</a>
-            <a href = '' className = 'btn btn-primary' target = '_blank'>Learn More</a>
+          <a href = 'https://www.digitext.ca/#/' className = 'btn' target = '_blank'>Learn More</a>
           </div>
         </article>
         <article className = 'portfolio__item'>
           <div className = 'portfolio__item-image'>
             <img src={img1} alt = ''/>
           </div>
-          <h3>put the achivement here</h3>
+          <h3>Pauly website for the school</h3>
           <div className = 'portfolio__item-cta'>
-            <a href = '' className = 'btn' target = '_blank'>Learn More</a>
             <a href = '' className = 'btn btn-primary' target = '_blank'>Learn More</a>
           </div>
         </article>
